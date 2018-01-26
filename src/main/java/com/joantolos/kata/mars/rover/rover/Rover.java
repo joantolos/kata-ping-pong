99a73bb4869ed38515df868c5a8de40e394121c7
joan.tolos <joan.tolos@clarivate.com>
2018-01-26 11:50:18 +0100

    Implemented movements forwards and backwards

--- src/main/java/com/joantolos/kata/mars/rover/rover/Rover.java
+++ src/main/java/com/joantolos/kata/mars/rover/rover/Rover.java
@@ -1,5 +1,6 @@
 package com.joantolos.kata.mars.rover.rover;
 
+import com.joantolos.kata.mars.rover.domain.Mars;
 import com.joantolos.kata.mars.rover.utils.Compass;
 import com.joantolos.kata.mars.rover.domain.Position;
 
@@ -19,17 +20,54 @@ public class Rover {
         return this.currentPosition;
     }
 
-    protected Position moveForward(Compass direction){
-        Position positionProposal = new Position(currentPosition.getX(), currentPosition.getY() + 1);
+    protected Boolean moveForward(){
+        Position positionProposal;
+        switch (currentDirection){
+            case NORTH:
+                positionProposal = new Position(this.currentPosition.getX(), incrementY());
+                break;
+            case SOUTH:
+                positionProposal = new Position(this.currentPosition.getX(), decrementY());
+                break;
+            case EAST:
+                positionProposal = new Position(incrementX(), this.currentPosition.getY());
+                break;
+            case WEST:
+                positionProposal = new Position(decrementX(), this.currentPosition.getY());
+                break;
+            default:
+                positionProposal = currentPosition;
+        }
         if(this.marsMap.isPositionAvailable(positionProposal)){
             this.currentPosition = positionProposal;
-            return positionProposal;
+            return true;
         }
-        return null;
+        return false;
     }
 
     protected Boolean moveBackward() {
-        return true;
+        Position positionProposal;
+        switch (currentDirection){
+            case NORTH:
+                positionProposal = new Position(this.currentPosition.getX(), decrementY());
+                break;
+            case SOUTH:
+                positionProposal = new Position(this.currentPosition.getX(), incrementY());
+                break;
+            case EAST:
+                positionProposal = new Position(decrementX(), this.currentPosition.getY());
+                break;
+            case WEST:
+                positionProposal = new Position(incrementX(), this.currentPosition.getY());
+                break;
+            default:
+                positionProposal = currentPosition;
+        }
+        if(this.marsMap.isPositionAvailable(positionProposal)){
+            this.currentPosition = positionProposal;
+            return true;
+        }
+        return false;
     }
 
     protected Boolean moveLeft() {
@@ -38,5 +76,37 @@ public class Rover {
 
     protected Boolean moveRight() {
         return true;
+    }
+
+    private Integer incrementY() {
+        Integer newYCandidate = this.currentPosition.getY() + 1;
+        if(newYCandidate > Mars.SIZE){
+            newYCandidate = 0;
+        }
+        return newYCandidate;
+    }
+
+    private Integer incrementX() {
+        Integer newXCandidate = this.currentPosition.getX() + 1;
+        if(newXCandidate > Mars.SIZE){
+            newXCandidate = 0;
+        }
+        return newXCandidate;
+    }
+
+    private Integer decrementY() {
+        Integer newYCandidate = this.currentPosition.getY() - 1;
+        if(newYCandidate < 0){
+            newYCandidate = Mars.SIZE;
+        }
+        return newYCandidate;
+    }
+
+    private Integer decrementX() {
+        Integer newXCandidate = this.currentPosition.getX() - 1;
+        if(newXCandidate < 0){
+            newXCandidate = Mars.SIZE;
+        }
+        return newXCandidate;
     }
 }
--- src/test/groovy/com/joantolos/kata/mars/rover/rover/RoverSpec.groovy
+++ src/test/groovy/com/joantolos/kata/mars/rover/rover/RoverSpec.groovy
@@ -1,39 +1,76 @@
 package com.joantolos.kata.mars.rover.rover
 
 import com.joantolos.kata.mars.rover.utils.Compass
-import spock.lang.Shared
 import spock.lang.Specification
 
 class RoverSpec extends Specification {
 
-    @Shared Rover rover
-
-    def setupSpec() {
-        rover = new Rover(0,0,Compass.NORTH)
-    }
-
     def 'Mars Rover should return its location' () {
+        given:
+        Rover rover = new Rover(0,0,Compass.NORTH)
+
         expect:
         rover.getPosition()
     }
 
-    def 'Mars Rover should move forward' () {
-        expect:
-        rover.moveForward(Compass.NORTH)
+    def 'Mars Rover should move forward when facing every direction, including edge positions' () {
+
+        given: 'Moving a new rover forward'
+        Rover rover = new Rover(startingX, startingY, direction)
+        rover.moveForward()
+
+        expect: 'The correct position'
+        rover.getPosition().x == expectingX
+        rover.getPosition().y == expectingY
+
+        where: 'The rover is facing all possible directions'
+        startingX   |   startingY   ||   direction          |   expectingX  |   expectingY
+        1           |   1           ||   Compass.NORTH      |   1           |   2
+        1           |   1           ||   Compass.SOUTH      |   1           |   0
+        1           |   1           ||   Compass.EAST       |   2           |   1
+        1           |   1           ||   Compass.WEST       |   0           |   1
+
+        9           |   9           ||   Compass.NORTH      |   9           |   0
+        0           |   0           ||   Compass.SOUTH      |   0           |   9
+        9           |   9           ||   Compass.EAST       |   0           |   9
+        0           |   0           ||   Compass.WEST       |   9           |   0
     }
 
-    def 'Mars Rover should move backward' () {
-        expect:
+    def 'Mars Rover should move backward when facing every direction, including edge positions' () {
+
+        given: 'Moving a new rover forward'
+        Rover rover = new Rover(startingX, startingY, direction)
         rover.moveBackward()
+
+        expect: 'The correct position'
+        rover.getPosition().x == expectingX
+        rover.getPosition().y == expectingY
+
+        where: 'The rover is facing all possible directions'
+        startingX   |   startingY   ||   direction          |   expectingX  |   expectingY
+        1           |   1           ||   Compass.NORTH      |   1           |   0
+        1           |   1           ||   Compass.SOUTH      |   1           |   2
+        1           |   1           ||   Compass.EAST       |   0           |   1
+        1           |   1           ||   Compass.WEST       |   2           |   1
+
+        9           |   9           ||   Compass.SOUTH      |   9           |   0
+        0           |   0           ||   Compass.NORTH      |   0           |   9
+        9           |   9           ||   Compass.WEST       |   0           |   9
+        0           |   0           ||   Compass.EAST       |   9           |   0
     }
 
     def 'Mars Rover should move left' () {
-        expect:
+
+        given: 'Moving a new rover left'
+        Rover rover = new Rover(1,1,Compass.EAST)
         rover.moveLeft()
+
     }
 
     def 'Mars Rover should move right' () {
-        expect:
+
+        given: 'Moving a new rover right'
+        Rover rover = new Rover(1,1,Compass.WEST)
         rover.moveRight()
     }
 }
