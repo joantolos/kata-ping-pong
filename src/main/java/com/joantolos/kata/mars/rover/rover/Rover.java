14eeb7878f47d3b83605c132151ef1810fe44f8e
joan.tolos <joan.tolos@clarivate.com>
2018-01-29 11:58:17 +0100

    Movements implemented on rover

--- src/main/java/com/joantolos/kata/mars/rover/rover/Rover.java
+++ src/main/java/com/joantolos/kata/mars/rover/rover/Rover.java
@@ -2,6 +2,7 @@ package com.joantolos.kata.mars.rover.rover;
 
 import com.joantolos.kata.mars.rover.domain.Mars;
 import com.joantolos.kata.mars.rover.domain.Position;
+import com.joantolos.kata.mars.rover.ui.UserInterface;
 import com.joantolos.kata.mars.rover.utils.Compass;
 
 public class Rover {
@@ -21,119 +22,76 @@ public class Rover {
     }
 
     protected Boolean move(Movements movement){
+        Position positionProposal = this.currentPosition;
         switch (movement) {
             case FORWARD:
-                return moveForward();
+                switch (currentDirection){
+                    case NORTH:
+                        positionProposal = new Position(this.currentPosition.getX(), this.increment(this.currentPosition.getY()));
+                        break;
+                    case SOUTH:
+                        positionProposal = new Position(this.currentPosition.getX(), this.decrement(this.currentPosition.getY()));
+                        break;
+                    case EAST:
+                        positionProposal = new Position(this.increment(this.currentPosition.getX()), this.currentPosition.getY());
+                        break;
+                    case WEST:
+                        positionProposal = new Position(this.decrement(this.currentPosition.getX()), this.currentPosition.getY());
+                        break;
+                }
+                break;
             case BACKWARD:
-                return moveBackward();
+                switch (currentDirection){
+                    case NORTH:
+                        positionProposal = new Position(this.currentPosition.getX(), this.decrement(this.currentPosition.getY()));
+                        break;
+                    case SOUTH:
+                        positionProposal = new Position(this.currentPosition.getX(), this.increment(this.currentPosition.getY()));
+                        break;
+                    case EAST:
+                        positionProposal = new Position(this.decrement(this.currentPosition.getX()), this.currentPosition.getY());
+                        break;
+                    case WEST:
+                        positionProposal = new Position(this.increment(this.currentPosition.getX()), this.currentPosition.getY());
+                        break;
+                }
+                break;
             case LEFT:
-                return moveLeft();
+                switch (currentDirection){
+                    case NORTH:
+                    case SOUTH:
+                        positionProposal = new Position(this.decrement(this.currentPosition.getX()), this.currentPosition.getY());
+                        break;
+                    case EAST:
+                    case WEST:
+                        positionProposal = new Position(this.increment(this.currentPosition.getX()), this.currentPosition.getY());
+                        break;
+                }
+                break;
             case RIGHT:
-                return moveRight();
+                switch (currentDirection){
+                    case NORTH:
+                    case SOUTH:
+                        positionProposal = new Position(this.increment(this.currentPosition.getX()), this.currentPosition.getY());
+                        break;
+                    case EAST:
+                    case WEST:
+                        positionProposal = new Position(this.decrement(this.currentPosition.getX()), this.currentPosition.getY());
+                        break;
+                }
+                break;
             case NORTH:
                 this.currentDirection = Compass.NORTH;
+                return true;
             case SOUTH:
                 this.currentDirection = Compass.SOUTH;
+                return true;
             case EAST:
                 this.currentDirection = Compass.EAST;
+                return true;
             case WEST:
                 this.currentDirection = Compass.WEST;
-        }
-        return false;
-    }
-
-    private Boolean moveForward(){
-        Position positionProposal;
-        switch (currentDirection){
-            case NORTH:
-                positionProposal = new Position(this.currentPosition.getX(), this.increment(this.currentPosition.getY()));
-                break;
-            case SOUTH:
-                positionProposal = new Position(this.currentPosition.getX(), this.decrement(this.currentPosition.getY()));
-                break;
-            case EAST:
-                positionProposal = new Position(this.increment(this.currentPosition.getX()), this.currentPosition.getY());
-                break;
-            case WEST:
-                positionProposal = new Position(this.decrement(this.currentPosition.getX()), this.currentPosition.getY());
-                break;
-            default:
-                positionProposal = currentPosition;
-        }
-        if(this.marsMap.isPositionAvailable(positionProposal)){
-            this.currentPosition = positionProposal;
-            return true;
-        }
-        return false;
-    }
-
-    private Boolean moveBackward() {
-        Position positionProposal;
-        switch (currentDirection){
-            case NORTH:
-                positionProposal = new Position(this.currentPosition.getX(), this.decrement(this.currentPosition.getY()));
-                break;
-            case SOUTH:
-                positionProposal = new Position(this.currentPosition.getX(), this.increment(this.currentPosition.getY()));
-                break;
-            case EAST:
-                positionProposal = new Position(this.decrement(this.currentPosition.getX()), this.currentPosition.getY());
-                break;
-            case WEST:
-                positionProposal = new Position(this.increment(this.currentPosition.getX()), this.currentPosition.getY());
-                break;
-            default:
-                positionProposal = currentPosition;
-        }
-        if(this.marsMap.isPositionAvailable(positionProposal)){
-            this.currentPosition = positionProposal;
-            return true;
-        }
-        return false;
-    }
-
-    private Boolean moveLeft() {
-        Position positionProposal;
-        switch (currentDirection){
-            case NORTH:
-                positionProposal = new Position(this.decrement(this.currentPosition.getX()), this.currentPosition.getY());
-                break;
-            case SOUTH:
-                positionProposal = new Position(this.decrement(this.currentPosition.getX()), this.currentPosition.getY());
-                break;
-            case EAST:
-                positionProposal = new Position(this.increment(this.currentPosition.getX()), this.currentPosition.getY());
-                break;
-            case WEST:
-                positionProposal = new Position(this.increment(this.currentPosition.getX()), this.currentPosition.getY());
-                break;
-            default:
-                positionProposal = currentPosition;
-        }
-        if(this.marsMap.isPositionAvailable(positionProposal)){
-            this.currentPosition = positionProposal;
-            return true;
-        }
-        return false;
-    }
-
-    private Boolean moveRight() {
-        Position positionProposal;
-        switch (currentDirection){
-            case NORTH:
-                positionProposal = new Position(this.increment(this.currentPosition.getX()), this.currentPosition.getY());
-                break;
-            case SOUTH:
-                positionProposal = new Position(this.increment(this.currentPosition.getX()), this.currentPosition.getY());
-                break;
-            case EAST:
-                positionProposal = new Position(this.decrement(this.currentPosition.getX()), this.currentPosition.getY());
-                break;
-            case WEST:
-                positionProposal = new Position(this.decrement(this.currentPosition.getX()), this.currentPosition.getY());
-                break;
-            default:
-                positionProposal = currentPosition;
+                return true;
         }
         if(this.marsMap.isPositionAvailable(positionProposal)){
             this.currentPosition = positionProposal;
@@ -143,20 +101,20 @@ public class Rover {
     }
 
     private Integer decrement(Integer point) {
-        Integer newPointCandidate;
-        newPointCandidate = point - 1;
-        if (newPointCandidate < 0) {
-            newPointCandidate = Mars.SIZE;
+        Integer pointCandidate;
+        pointCandidate = point - 1;
+        if (pointCandidate < 0) {
+            pointCandidate = Mars.SIZE;
         }
-        return newPointCandidate;
+        return pointCandidate;
     }
 
     private Integer increment(Integer point) {
-        Integer newPointCandidate;
-        newPointCandidate = point + 1;
-        if (newPointCandidate > Mars.SIZE) {
-            newPointCandidate = 0;
+        Integer pointCandidate;
+        pointCandidate = point + 1;
+        if (pointCandidate > Mars.SIZE) {
+            pointCandidate = 0;
         }
-        return newPointCandidate;
+        return pointCandidate;
     }
 }
