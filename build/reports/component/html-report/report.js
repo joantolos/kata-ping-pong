c196e82cf7d49384185f3ed8594aa0eed6fc6f52
Joan Tolós López <joan@Joans-MBP-2.mm-ads.com>
2019-11-27 16:12:58 +0100

    Updating gradle version

---
+++ build/reports/component/html-report/report.js
@@ -0,0 +1,199 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("game.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Game",
+  "description": "",
+  "id": "game",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 421445880,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Checking if the service does ping",
+  "description": "",
+  "id": "game;checking-if-the-service-does-ping",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@Ping"
+    }
+  ]
+});
+formatter.step({
+  "line": 5,
+  "name": "I perform a \"GET\" to \"http://localhost:7111/ping\" end point",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "the response at the json path \"move\" includes \"Ping!\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "GET",
+      "offset": 13
+    },
+    {
+      "val": "http://localhost:7111/ping",
+      "offset": 22
+    }
+  ],
+  "location": "PingPongAcceptance.java:25"
+});
+formatter.result({
+  "duration": 1095960443,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "move",
+      "offset": 31
+    },
+    {
+      "val": "Ping!",
+      "offset": 47
+    }
+  ],
+  "location": "PingPongAcceptance.java:29"
+});
+formatter.result({
+  "duration": 615265951,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 350518,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 9,
+  "name": "Checking if the service does pong",
+  "description": "",
+  "id": "game;checking-if-the-service-does-pong",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 8,
+      "name": "@Pong"
+    }
+  ]
+});
+formatter.step({
+  "line": 10,
+  "name": "I perform a \"GET\" to \"http://localhost:7111/pong\" end point",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 11,
+  "name": "the response at the json path \"move\" includes \"Pong!\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "GET",
+      "offset": 13
+    },
+    {
+      "val": "http://localhost:7111/pong",
+      "offset": 22
+    }
+  ],
+  "location": "PingPongAcceptance.java:25"
+});
+formatter.result({
+  "duration": 16561384,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "move",
+      "offset": 31
+    },
+    {
+      "val": "Pong!",
+      "offset": 47
+    }
+  ],
+  "location": "PingPongAcceptance.java:29"
+});
+formatter.result({
+  "duration": 10720193,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 324903,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 14,
+  "name": "Checking if the service does ping or pong when playing",
+  "description": "",
+  "id": "game;checking-if-the-service-does-ping-or-pong-when-playing",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 13,
+      "name": "@Play"
+    }
+  ]
+});
+formatter.step({
+  "line": 15,
+  "name": "I perform a \"GET\" to \"http://localhost:7111/play\" end point",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 16,
+  "name": "the response at the json path \"move\" includes \"Ping!\" or \"Pong!\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "GET",
+      "offset": 13
+    },
+    {
+      "val": "http://localhost:7111/play",
+      "offset": 22
+    }
+  ],
+  "location": "PingPongAcceptance.java:25"
+});
+formatter.result({
+  "duration": 16596431,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "move",
+      "offset": 31
+    },
+    {
+      "val": "Ping!",
+      "offset": 47
+    },
+    {
+      "val": "Pong!",
+      "offset": 58
+    }
+  ],
+  "location": "PingPongAcceptance.java:32"
+});
+formatter.result({
+  "duration": 18391967,
+  "status": "passed"
+});
+});
